c2961a2bda7efcf99eee3f6d3174ac2d5ce6af65
bikramia <bikram.rhce@gmail.com>
2023-12-26 22:34:46 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
+import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
+import ExpenseForm from "./expense-tracker/components/ExpenseForm";
+
+
+
+function App() {
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 80, category: "Utilities" },
+    { id: 2, description: "bbb", amount: 40, category: "Utilities" },
+    { id: 3, description: "ccc", amount: 100, category: "Utilities" },
+    { id: 4, description: "ddd", amount: 70, category: "Utilities" },
+  ]);
+
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
+
+  return (
+    <div>
+      <div className="mb-5">
+        <ExpenseForm  onSubmit={expense => setExpenses([...expenses, {...expense, id: expenses.length + 1}])}/>
+      </div>
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        />
+      </div>
+      <ExpenseList
+        expenses={visibleExpenses}
+        onDelete={(id) =>
+          setExpenses(expenses.filter((e: { id: number }) => e.id !== id))
+        }
+      />
+    </div>
+  );
+}
+
+export default App;
